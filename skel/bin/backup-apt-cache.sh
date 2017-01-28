fb54e265cf382bc8e676c6d3129b5a4fe58fd571
johnko <git@johnko.ca>
2017-01-28 04:37:34 -0500

    use HOME

--- skel/bin/backup-apt-cache.sh
+++ skel/bin/backup-apt-cache.sh
@@ -5,12 +5,11 @@ set -x
 . ${HOME}/.env.sh
 
 if [ -n "${RSYNC_CACHE_SERVER}" ]; then
-  mkdir -p /home/${NEWUSER}/.apt-cache/apt/archives
-  rsync -virtP --exclude lock --exclude partial /var/lib/apt/lists/ /home/${NEWUSER}/.apt-cache/lists/
-  rsync -virtP --exclude lock --exclude partial /var/cache/apt/ /home/${NEWUSER}/.apt-cache/apt/
-  chown -R ${NEWUSER} /home/${NEWUSER}/.apt-cache
-  cd /home/${NEWUSER}/.apt-cache/apt/archives
-  dpkg -l | awk '$1~/ii/{print "sudo -u ${NEWUSER} apt download "$2}' | sh -ex
+  mkdir -p ${HOME}/.apt-cache/apt/archives
+  rsync -virtP --exclude lock --exclude partial /var/lib/apt/lists/ ${HOME}/.apt-cache/lists/
+  rsync -virtP --exclude lock --exclude partial /var/cache/apt/ ${HOME}/.apt-cache/apt/
+  cd ${HOME}/.apt-cache/apt/archives
+  dpkg -l | awk '$1~/ii/{print "apt download "$2}' | sh -ex
   cd -
-  rsync -virtP --exclude lock --exclude partial /home/${NEWUSER}/.apt-cache/ ${RSYNC_CACHE_SERVER}/
+  rsync -virtP --exclude lock --exclude partial ${HOME}/.apt-cache/ ${RSYNC_CACHE_SERVER}/
 fi
