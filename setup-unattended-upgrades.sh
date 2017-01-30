580cacda4949b201e224eb965f34a0403901421b
johnko <git@johnko.ca>
2017-01-29 20:14:17 -0500

    sed then check if we still need to add

--- setup-unattended-upgrades.sh
+++ setup-unattended-upgrades.sh
@@ -9,26 +9,21 @@ apt install -y unattended-upgrades
 
 OUTFILE="/etc/apt/apt.conf.d/50unattended-upgrades"
 [ -f ${OUTFILE} -a ! -f ${OUTFILE}.original ] && cp -a ${OUTFILE} ${OUTFILE}.original
-if grep -q '^//.*"${distro_id}:${distro_codename}-updates' ${OUTFILE}; then
-  sed -i 's,^//.*"${distro_id}:${distro_codename}-updates.*,"${distro_id}:${distro_codename}-updates";,' ${OUTFILE}
-fi
-if grep -q "^//Unattended-Upgrade::MinimalSteps" ${OUTFILE}; then
-  sed -i 's,^//Unattended-Upgrade::MinimalSteps.*,Unattended-Upgrade::MinimalSteps "true";,' ${OUTFILE}
-else
+sed -i 's,^//.*"${distro_id}:${distro_codename}-updates.*,"${distro_id}:${distro_codename}-updates";,' ${OUTFILE}
+sed -i 's,^//Unattended-Upgrade::MinimalSteps.*,Unattended-Upgrade::MinimalSteps "true";,' ${OUTFILE}
+if ! grep -q "^Unattended-Upgrade::MinimalSteps" ${OUTFILE}; then
   cat >>${OUTFILE} <<EOF
 Unattended-Upgrade::MinimalSteps "true";
 EOF
 fi
-if grep -q "^//Unattended-Upgrade::Mail " ${OUTFILE}; then
-  sed -i "s,^//Unattended-Upgrade::Mail .*,Unattended-Upgrade::Mail \"${MAILTO}\";," ${OUTFILE}
-else
+sed -i "s,^//Unattended-Upgrade::Mail .*,Unattended-Upgrade::Mail \"${MAILTO}\";," ${OUTFILE}
+if ! grep -q "^Unattended-Upgrade::Mail " ${OUTFILE}; then
   cat >>${OUTFILE} <<EOF
 Unattended-Upgrade::Mail "${MAILTO}";
 EOF
 fi
-if grep -q "^//Unattended-Upgrade::MailOnlyOnError" ${OUTFILE}; then
-  sed -i 's,^//Unattended-Upgrade::MailOnlyOnError .*,Unattended-Upgrade::MailOnlyOnError "false";,' ${OUTFILE}
-else
+sed -i 's,^//Unattended-Upgrade::MailOnlyOnError .*,Unattended-Upgrade::MailOnlyOnError "false";,' ${OUTFILE}
+if ! grep -q "^Unattended-Upgrade::MailOnlyOnError" ${OUTFILE}; then
   cat >>${OUTFILE} <<EOF
 Unattended-Upgrade::MailOnlyOnError "false";
 EOF
@@ -36,16 +31,14 @@ fi
 
 OUTFILE="/etc/apt/apt.conf.d/10periodic"
 [ -f ${OUTFILE} -a ! -f ${OUTFILE}.original ] && cp -a ${OUTFILE} ${OUTFILE}.original
-if grep -q "^APT::Periodic::Download-Upgradeable-Packages" ${OUTFILE}; then
-  sed -i 's,^APT::Periodic::Download-Upgradeable-Packages.*,APT::Periodic::Download-Upgradeable-Packages "1";,' ${OUTFILE}
-else
+sed -i 's,^APT::Periodic::Download-Upgradeable-Packages.*,APT::Periodic::Download-Upgradeable-Packages "1";,' ${OUTFILE}
+if ! grep -q "^APT::Periodic::Download-Upgradeable-Packages" ${OUTFILE}; then
   cat >>${OUTFILE} <<EOF
 APT::Periodic::Download-Upgradeable-Packages "1";
 EOF
 fi
-if grep -q "^APT::Periodic::Update-Package-Lists" ${OUTFILE}; then
-  sed -i 's,^APT::Periodic::Update-Package-Lists.*,APT::Periodic::Update-Package-Lists "1";,' ${OUTFILE}
-else
+sed -i 's,^APT::Periodic::Update-Package-Lists.*,APT::Periodic::Update-Package-Lists "1";,' ${OUTFILE}
+if ! grep -q "^APT::Periodic::Update-Package-Lists" ${OUTFILE}; then
   cat >>${OUTFILE} <<EOF
 APT::Periodic::Update-Package-Lists "1";
 EOF
@@ -53,16 +46,14 @@ fi
 
 OUTFILE="/etc/apt/apt.conf.d/20auto-upgrades"
 [ -f ${OUTFILE} -a ! -f ${OUTFILE}.original ] && cp -a ${OUTFILE} ${OUTFILE}.original
-if grep -q "^APT::Periodic::Update-Package-Lists" ${OUTFILE}; then
-  sed -i 's,^APT::Periodic::Update-Package-Lists.*,APT::Periodic::Update-Package-Lists "1";,' ${OUTFILE}
-else
+sed -i 's,^APT::Periodic::Update-Package-Lists.*,APT::Periodic::Update-Package-Lists "1";,' ${OUTFILE}
+if ! grep -q "^APT::Periodic::Update-Package-Lists" ${OUTFILE}; then
   cat >>${OUTFILE} <<EOF
 APT::Periodic::Update-Package-Lists "1";
 EOF
 fi
-if grep -q "^APT::Periodic::Unattended-Upgrade" ${OUTFILE}; then
-  sed -i 's,^APT::Periodic::Unattended-Upgrade.*,APT::Periodic::Unattended-Upgrade "1";,' ${OUTFILE}
-else
+sed -i 's,^APT::Periodic::Unattended-Upgrade.*,APT::Periodic::Unattended-Upgrade "1";,' ${OUTFILE}
+if ! grep -q "^APT::Periodic::Unattended-Upgrade" ${OUTFILE}; then
   cat >>${OUTFILE} <<EOF
 APT::Periodic::Unattended-Upgrade "1";
 EOF
