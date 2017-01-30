6fd528800bbca34d1f81bd4b594f78a366ed2dd1
johnko <git@johnko.ca>
2017-01-29 20:07:56 -0500

    remove grep -q

--- setup-networkmanager-dns.sh
+++ setup-networkmanager-dns.sh
@@ -4,7 +4,6 @@ set -x
 
 OUTFILE="/etc/NetworkManager/NetworkManager.conf"
 if [ -f ${OUTFILE} ]; then
-  grep -q '^dns=dnsmasq' ${OUTFILE} && sed -i 's;^dns=dnsmasq;#dns=dnsmasq;' ${OUTFILE}
+  sed -i 's;^dns=dnsmasq;#dns=dnsmasq;' ${OUTFILE}
   #service network-manager restart
 fi
-
