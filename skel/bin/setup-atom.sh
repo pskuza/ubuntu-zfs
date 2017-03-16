d8ca46d473f1b1f80aa8aeedf4f7277bdb6c7db0
John Ko <git@johnko.ca>
2017-03-15 22:21:58 -0400

    Update setup-atom.sh

--- skel/bin/setup-atom.sh
+++ skel/bin/setup-atom.sh
@@ -3,23 +3,20 @@ set -e
 set -x
 
 if ! which atom; then
-    [ ! -e ~/atom.deb ] && curl -o ~/atom.deb -L https://atom.io/download/deb
-    sudo dpkg -i ~/atom.deb
+  FILE=atom.deb
+  [ ! -e ~/${FILE} ] && curl -o ~/${FILE} -L https://atom.io/download/deb
+  sudo dpkg -i ~/${FILE}
 fi
 
 which shellcheck || sudo apt install -y shellcheck
-
 which go || sudo apt install -y golang golang-go
-
 which git || sudo apt install -y git
 
 apm install linter
-
 apm install linter-shellcheck
 
 export GOPATH=${HOME}/go
 PATH="${PATH}:${GOROOT}/bin:${GOPATH}/bin"
-
 go get -u github.com/mvdan/sh/cmd/shfmt
 
 apm install format-shell
