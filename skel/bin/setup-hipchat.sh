ca992ada7bba79c56028cb1e497061a6c3f3f6be
John Ko <git@johnko.ca>
2017-03-15 22:16:46 -0400

    Update setup-hipchat.sh

--- skel/bin/setup-hipchat.sh
+++ skel/bin/setup-hipchat.sh
@@ -2,10 +2,14 @@
 set -e
 set -x
 
-[ ! -e /etc/apt/sources.list.d/atlassian-hipchat4.list ] && sudo tee /etc/apt/sources.list.d/atlassian-hipchat4.list <<EOF
+SOURCE=/etc/apt/sources.list.d/atlassian-hipchat4.list
+
+[ ! -e ${SOURCE} ] && sudo tee ${SOURCE} <<EOF
 deb https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client $(lsb_release -c -s) main
 EOF
+
 wget -O - https://atlassian.artifactoryonline.com/atlassian/api/gpg/key/public | sudo apt-key add -
+
 sudo apt update
 
 which hipchat4 || sudo apt install -y hipchat4
