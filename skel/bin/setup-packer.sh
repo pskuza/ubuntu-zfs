6cb0436c57b5c67317f80eab63c88c360308682f
John Ko <git@johnko.ca>
2017-03-15 22:11:04 -0400

    apt install if missing

--- skel/bin/setup-packer.sh
+++ skel/bin/setup-packer.sh
@@ -2,6 +2,8 @@
 set -e
 set -x
 
+which vagrant || sudo apt install -y vagrant
+
 if [ ! -e /usr/bin/packer ]; then
   curl -L https://releases.hashicorp.com/packer/0.12.3/packer_0.12.3_linux_amd64.zip > ~/packer_0.12.3_linux_amd64.zip
   unzip ~/packer_0.12.3_linux_amd64.zip
