42437b93afe17dca68863dabd7b29410bdc43e7c
John Ko <git@johnko.ca>
2017-03-15 22:09:58 -0400

    apt install if missing

--- skel/bin/setup-dapper.sh
+++ skel/bin/setup-dapper.sh
@@ -2,6 +2,9 @@
 set -e
 set -x
 
+which go || sudo apt install -y golang golang-go
+which git || sudo apt install -y git
+
 if [ ! -e /usr/bin/dapper ]; then
   export GOPATH=${HOME}/go
   PATH="${PATH}:${GOROOT}/bin:${GOPATH}/bin"
