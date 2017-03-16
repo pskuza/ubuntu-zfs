a01a3b0dcc268677b276e1d0166e035981339eef
John Ko <git@johnko.ca>
2017-03-15 22:05:39 -0400

    Create setup-packer.sh

---
+++ skel/bin/setup-packer.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+if [ ! -e /usr/bin/packer ]; then
+  curl -L https://releases.hashicorp.com/packer/0.12.3/packer_0.12.3_linux_amd64.zip > ~/packer_0.12.3_linux_amd64.zip
+  unzip ~/packer_0.12.3_linux_amd64.zip
+  sudo mv packer /usr/bin/packer
+fi
