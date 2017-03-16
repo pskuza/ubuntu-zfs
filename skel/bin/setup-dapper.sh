37a033f2c35059807739c1f2b2b92a20a2399ad3
John Ko <git@johnko.ca>
2017-03-15 22:22:25 -0400

    Update setup-dapper.sh

--- skel/bin/setup-dapper.sh
+++ skel/bin/setup-dapper.sh
@@ -8,7 +8,6 @@ which git || sudo apt install -y git
 if [ ! -e /usr/bin/dapper ]; then
   export GOPATH=${HOME}/go
   PATH="${PATH}:${GOROOT}/bin:${GOPATH}/bin"
-
   go get github.com/rancher/dapper
   sudo mv ~/go/bin/dapper /usr/bin/dapper
 fi
