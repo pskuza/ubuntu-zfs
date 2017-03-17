8a6227a5d70d6563a8282af1fb26e97c2c074c2b
johnko <git@johnko.ca>
2017-03-17 16:56:32 -0400

    mount /etc/passwd readonly to parse process owner

--- skel/bin/glances.sh
+++ skel/bin/glances.sh
@@ -9,6 +9,6 @@ NETWORK_OPT="--net host"
 
 docker rm -f ${CONTAINER_NAME} || true
 docker pull ${IMAGE_NAME}
-docker run -d --restart=always ${NETWORK_OPT} -e GLANCES_OPT="-w" -v /var/run/docker.sock:/var/run/docker.sock:ro --pid host --name ${CONTAINER_NAME} ${IMAGE_NAME}
+docker run -d --restart=always ${NETWORK_OPT} -e GLANCES_OPT="-w" -v /etc/passwd:/etc/passwd:ro -v /var/run/docker.sock:/var/run/docker.sock:ro --pid host --name ${CONTAINER_NAME} ${IMAGE_NAME}
 
 echo "visit http://localhost:61208/"
