bab6b71f0e1828f1598709d897963326f9906cbd
johnko <git@johnko.ca>
2017-03-17 16:36:32 -0400

    update docker run scripts

--- skel/bin/glances.sh
+++ skel/bin/glances.sh
@@ -2,7 +2,13 @@
 set -e
 set -x
 
-#docker run -d --restart="always" -p 61208-61209:61208-61209 -e GLANCES_OPT="-w" -v /var/run/docker.sock:/var/run/docker.sock:ro --pid host nicolargo/glances
-docker run -d --restart="always" --net host -e GLANCES_OPT="-w" -v /var/run/docker.sock:/var/run/docker.sock:ro --pid host nicolargo/glances
+IMAGE_NAME=nicolargo/glances
+CONTAINER_NAME=glances
+NETWORK_OPT="-p 61208-61209:61208-61209"
+NETWORK_OPT="--net host"
+
+docker rm -f ${CONTAINER_NAME} || true
+docker pull ${IMAGE_NAME}
+docker run -d --restart=always ${NETWORK_OPT} -e GLANCES_OPT="-w" -v /var/run/docker.sock:/var/run/docker.sock:ro --pid host --name ${CONTAINER_NAME} ${IMAGE_NAME}
 
 echo "visit http://localhost:61208/"
--- skel/bin/rancher.sh
+++ skel/bin/rancher.sh
@@ -2,6 +2,12 @@
 set -e
 set -x
 
-docker run -d --restart=always -p 8080:8080 rancher/server
+IMAGE_NAME=rancher/server
+CONTAINER_NAME=rancher_server
+NETWORK_OPT="-p 8080:8080"
+
+docker rm -f ${CONTAINER_NAME} || true
+docker pull ${IMAGE_NAME}
+docker run -d --restart=always ${NETWORK_OPT} --name ${CONTAINER_NAME} ${IMAGE_NAME}
 
 echo "visit http://localhost:8080/"
