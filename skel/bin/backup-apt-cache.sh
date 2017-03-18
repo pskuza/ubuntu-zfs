07329e982d76699ec944e3883bbe75d9fdadf9a5
John Ko <git@johnko.ca>
2017-03-18 03:55:21 +0000

    Update backup-apt-cache.sh

--- skel/bin/backup-apt-cache.sh
+++ skel/bin/backup-apt-cache.sh
@@ -4,12 +4,13 @@ set -x
 
 . ${HOME}/.env.sh
 
+mkdir -p ${HOME}/.apt-cache/apt/archives
+rsync -virtP --exclude lock --exclude partial /var/lib/apt/lists/ ${HOME}/.apt-cache/lists/
+rsync -virtP --exclude lock --exclude partial /var/cache/apt/ ${HOME}/.apt-cache/apt/
+cd ${HOME}/.apt-cache/apt/archives
+dpkg -l | awk '$1~/ii/{print "apt download "$2}' | sh -ex
+cd -
+
 if [ -n "${RSYNC_CACHE_SERVER}" ]; then
-  mkdir -p ${HOME}/.apt-cache/apt/archives
-  rsync -virtP --exclude lock --exclude partial /var/lib/apt/lists/ ${HOME}/.apt-cache/lists/
-  rsync -virtP --exclude lock --exclude partial /var/cache/apt/ ${HOME}/.apt-cache/apt/
-  cd ${HOME}/.apt-cache/apt/archives
-  dpkg -l | awk '$1~/ii/{print "apt download "$2}' | sh -ex
-  cd -
   rsync -virtP --exclude lock --exclude partial ${HOME}/.apt-cache/ ${RSYNC_CACHE_SERVER}/
 fi
