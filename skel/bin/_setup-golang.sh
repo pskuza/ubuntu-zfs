76afb5e8a431101d2aeb65f73f1a5be807e7e047
johnko <git@johnko.ca>
2017-03-18 23:22:03 -0400

    follow redirect

--- skel/bin/_setup-golang.sh
+++ skel/bin/_setup-golang.sh
@@ -3,12 +3,12 @@ set -e
 set -x
 
 VERSION=1.8
-FILE=go${VERSION}.linux-amd64.tar.gz
 
 export GOPATH=${HOME}/go
 PATH="${PATH}:${GOROOT}/bin:${GOPATH}/bin:/usr/local/go/bin"
 
 if ! which go ; then
-  [ ! -e ~/${FILE} ] && curl -o ~/${FILE} https://storage.googleapis.com/golang/${FILE}
+  FILE=go${VERSION}.linux-amd64.tar.gz
+  [ ! -e ~/${FILE} ] && curl -o ~/${FILE} -L https://storage.googleapis.com/golang/${FILE}
   sudo tar -C /usr/local -xzf ~/${FILE}
 fi
